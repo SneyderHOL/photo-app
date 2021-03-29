15988f79d995d53a433486d43a89470bea44f926
SneyderHOL <1507@holbertonschool.com>
2021-03-29 14:26:37 -0500

    Add configuration for production with carrierwave

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,10 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => Rails.application.fog_credentials.aws[aws_access_key_id],
+      :aws_secret_access_key => Rails.application.fog_credentials.aws[aws_secret_access_key_id],.
+    }
+    config.fog_directory = Rails.application.fog_credentials.aws[s3_bucket_dir]
+  end
+end
