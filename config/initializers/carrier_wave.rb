71477a31811f6d332d1c9fa3dccbd67f126b14c1
SneyderHOL <1507@holbertonschool.com>
2021-03-29 14:29:15 -0500

    Fix typo

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => Rails.application.fog_credentials.aws[aws_access_key_id],
-      :aws_secret_access_key => Rails.application.fog_credentials.aws[aws_secret_access_key_id],.
+      :aws_secret_access_key => Rails.application.fog_credentials.aws[aws_secret_access_key_id],
     }
     config.fog_directory = Rails.application.fog_credentials.aws[s3_bucket_dir]
   end
